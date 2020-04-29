2497ee12ab5d620b9baaf70e84972b4864408aeb
AlejandroDeheza <ale.deheza@gmail.com>
2020-04-29 18:26:38 -0300

    Agrego archivos al gitignore

--- src/test/java/Prueba/testDummyDesign.java
+++ src/test/java/Prueba/testDummyDesign.java
@@ -10,33 +10,32 @@ import org.junit.Test;
 public class testDummyDesign {
 
 	private DummyDesign dummy = new DummyDesign();
-	
+
 	@Test
 	public void testIntegrante1() {
-		Assert.assertEquals(1,dummy.integrante1());
+		Assert.assertEquals(1, dummy.integrante1());
 	}
-	
+
 	@Test
 	public void testIntegrante2() {
-		Assert.assertEquals(2,dummy.integrante2());
+		Assert.assertEquals(2, dummy.integrante2());
 	}
 
 	@Test
 	public void testIntegrante3() {
-		//dummy = new DummyDesign();
+		// dummy = new DummyDesign();
 		Assert.assertEquals(3, dummy.integrante3());
 	}
-	
+
 	@Test
 	public void testIntegrante4() {
-		//dummy = new DummyDesign();
+		// dummy = new DummyDesign();
 		Assert.assertEquals(4, dummy.integrante4());
 	}
-	
+
 	@Test
 	public void testIntegrante5() {
-		Assert.assertEquals(5,dummy.integrante5());
+		Assert.assertEquals(5, dummy.integrante5());
 	}
 
 }
-
