f3d4a8171a0ea2cdd33a6117f4e25ee3d1a24f59
AlejandroDeheza <ale.deheza@gmail.com>
2020-05-15 01:11:20 -0300

    Agregado al commit anterior

--- TP GeSoc/src/main/java/paymentMethods/Master.java
+++ TP GeSoc/src/main/java/paymentMethods/Master.java
@@ -3,6 +3,6 @@ package paymentMethods;
 import model.IDMedioDePago;
 import model.TarjetaDeCredito;
 
-public class master extends TarjetaDeCredito {
+public class Master extends TarjetaDeCredito {
 	IDMedioDePago idMedioDePago = IDMedioDePago.MASTER;
 }
